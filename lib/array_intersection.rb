45ba31a603ae4d8318bb1771eafe3c38d56f5858
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-03 07:36:31 -0700

    sorting / binary search way of finding intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -2,24 +2,28 @@
 def intersection(a, b)
 
   #MY SOLUTION
-  if a == nil || b == nil
-    return []
-  end
+  # if a == nil || b == nil
+  #   return []
+  # end
+  #
+  # if a.length == 0 || b.length == 0
+  #   return []
+  # end
+  #
+  # if a.length < b.length
+  #   c = a - b
+  #   aintersect = a - c
+  #   return aintersect
+  # else
+  #   d = b - a
+  #   bintersect = b - d
+  #   return bintersect
+  # end
 
-  if a.length == 0 || b.length == 0
-    return []
-  end
+  #elem2 == elem1 && elem1 != intersect[intersect.length -1]
 
-  if a.length < b.length
-    c = a - b
-    aintersect = a - c
-    return aintersect
-  else
-    d = b - a
-    bintersect = b - d
-    return bintersect
-  end
 
+  # [21, 34, 42, 70, 78, 90]
   #RUBY WAY
   # # in ruby, the & is also the intersection
   #  a & b
@@ -58,4 +62,71 @@ def intersection(a, b)
   # end
   #
   # return intersect
+
+
+  #Another way: Merge sort / quicksort on longer array
+  #Binary search on longer array to search for elem in common with each elem of shorter array
+  return [] if a == nil || b == nil || a.length == 0 || b.length == 0
+
+  if a.length <= b.length
+    shorter = a
+    longer = b
+  else
+    shorter = b
+    longer = a
+  end
+
+  sort(longer, longer.length)
+
+  intersect = []
+
+  shorter.each do |i|
+    common = binary_search(longer, longer.length, i)
+    if common
+      intersect << common
+    end
+  end
+
+  return intersect
+end
+
+#helper for sorting / binary way of finding intersection
+def sort(array, length)
+  length.times do |index| # outer loop - n elements
+    min_index = index # assume index is where the next minimally value is
+    temp_index = index+1 # compare with values at index+1 to length-1
+    while temp_index < length # inner loop - n-1 elements
+      if array[temp_index] < array[min_index] # found a new minimum, update min_index
+        min_index = temp_index
+      end
+      temp_index += 1 # move to next index
+    end
+    if min_index != index # next minimum value is not at current index, swap
+      temp = array[min_index]
+      array[min_index] = array[index]
+      array[index] = temp
+    end
+  end
+end
+
+#helper for sorting / binary way of finding intersection
+def binary_search(array, length, value_to_find)
+  middle = length / 2
+  low = 0
+  high = length - 1
+
+  while low <= high
+
+    if array[middle] == value_to_find
+      return value_to_find
+    elsif array[middle] < value_to_find
+      low = middle + 1
+    else
+      high = middle - 1
+    end
+
+    middle = (low + high) / 2
+  end
+
+  return false
 end
