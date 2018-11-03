8ee8100be3d960a1c4cc2363ef1839b555414d98
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-03 06:14:32 -0700

    did it the shruti way

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,7 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(a, b)
 
+  #MY SOLUTION
   if a == nil || b == nil
     return []
   end
@@ -19,6 +20,42 @@ def intersection(a, b)
     return bintersect
   end
 
+  #RUBY WAY
   # # in ruby, the & is also the intersection
   #  a & b
+
+  # #SHRUTI WAY
+  #
+  # # using the shorter array, create a hash where the integers are the keys
+  # # set the value for the key to true for integers in the array
+  #
+  # return [] if a == nil || b == nil || a.length == 0 || b.length == 0
+  #
+  # if a.length <= b.length
+  #   shorter = a
+  #   longer = b
+  # else
+  #   shorter = b
+  #   longer = a
+  # end
+  #
+  # i = 0
+  # hash = {}
+  #
+  # while i < shorter.length do
+  #   hash[shorter[i]] = true
+  #   i += 1
+  # end
+  #
+  # i = 0
+  # intersect = []
+  #
+  # while i < longer.length do
+  #   if hash[longer[i]]
+  #     intersect << longer[i]
+  #   end
+  #   i += 1
+  # end
+  #
+  # return intersect
 end
