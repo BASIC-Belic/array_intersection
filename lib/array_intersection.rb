9f84e6df895d4b40e12d2fe8813c45ed9baf461c
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-03 05:43:28 -0700

    set intersection tests pass

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,22 @@
 # Creates a new array to return the intersection of the two input arrays
-def intersection(array1, array2)
-  raise NotImplementedError
+def intersection(a, b)
+
+  if a == nil || b == nil
+    return []
+  end
+
+  if a.length == 0 || b.length == 0
+    return []
+  end
+
+  c = a - b
+
+  aintersect = a - c
+
+  return aintersect
+
+  #similarly:
+  # d = b - a
+  #bintersect = b - d
+  #this should be equal to aintersect
 end
